ace9f83a15d2920f96f8c45aad037a48617d6157
bucakhasan <bucakhasan@icloud.com>
2016-12-08 12:58:36 +0300

    deneme

---
+++ cnsl-Toplama/cnsl-Toplama/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnsl_Toplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Klavyeden girilen iki sayının toplamını hesaplayıp ekranda gösteren console uygulamasını yazınız.
+            Console.Write("1. sayı girin : ");
+            string A = Console.ReadLine();
+            Console.Write("2. sayı girin : ");
+            string B = Console.ReadLine();
+            //Console.Write(A + B);
+            int S1, S2;
+            S1 = int.Parse(A);
+            S2 = int.Parse(B);
+            //Console.Write("Toplam = " + (S1 + S2));
+            int T = S1 + S2;
+            //Console.Write("Toplam = " + T);
+            //Console.Write("1.sayı = {0}, 2.sayı = {1}", S1, S2);
+            Console.Write("{0} + {1} = {2}", S1, S2, S1 + S2);
+            Console.ReadKey();
+        }
+    }
+}
